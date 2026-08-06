d896119d16950c5320f4f57992b16cec63f4c9d9
Rafał Łukawski <rs.lukawski@gmail.com>
2026-08-06 18:00:48 +0200

    fix: update width property for responsive design in ProjectsSection component

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -352,7 +352,7 @@ export function ProjectsSection() {
                   onClick={() => handleScreenshotClick(project.id, 0)}
                   sx={{
                     position: "relative",
-                    width: { xs: 220, md: 300 },
+                    width: { xs: 'auto', md: 300 },
                     height: "auto",
                     cursor: "pointer",
                     border: `2px solid ${grey[300]}`,
